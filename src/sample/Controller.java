1d12fcb44fb43147063af0147631059a3b066817
imranspahic <62642796+imranspahic@users.noreply.github.com>
2021-03-23 15:17:56 +0100

    funkcija sumaCifara

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,10 +4,12 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
     public TextField textField;
+    public Label labela;
 
     @FXML
     public  void initialize() {
@@ -23,4 +25,25 @@ public class Controller {
         });
     }
 
+    public void savucaj(ActionEvent actionEvent) {
+        int broj = Integer.parseInt(textField.getText());
+        String tekst = "";
+        for (int i = 1; i < broj; i++) {
+            int suma = sumaCifara(i);
+            if(i%suma == 0) tekst+= " " + i;
+        }
+        if(!tekst.isEmpty()) {
+            String labelaTekst = "Brojevi su djeljivi sa svojim ciframa između 1 i n su: ";
+            labela.setText(labelaTekst + tekst);
+        }
+    }
+
+    private int sumaCifara(int broj) {
+        int sumaCifara=0;
+        do {
+            sumaCifara+= broj%10;
+            broj/=10;
+        } while(broj!=0);
+        return sumaCifara;
+    }
 }
